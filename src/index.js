98dda96f6ba96e39565c04723775f2589bd1416b
CharlesMariga <charlesmariga37@gmail.com>
2023-12-09 11:45:23 +0300

    improving reusability with props

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 
 import StarRating from "./StartRating";
 
+function Test() {
+  const [rating, setRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating
+        color="blue"
+        defaultRating={rating}
+        size={36}
+        onSetRating={setRating}
+      />
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating />
+    <StarRating
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+    />
+    <Test />
   </React.StrictMode>
 );
