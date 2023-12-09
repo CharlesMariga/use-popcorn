8578e299fe3d2e58cbf95407b53f0767d0c63499
CharlesMariga <charlesmariga37@gmail.com>
2023-12-09 11:54:18 +0300

    proptyles

--- src/index.js
+++ src/index.js
@@ -1,33 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 
 import StarRating from "./StartRating";
 
-function Test() {
-  const [rating, setRating] = useState(0);
-
-  return (
-    <div>
-      <StarRating
-        color="blue"
-        defaultRating={rating}
-        size={36}
-        onSetRating={setRating}
-      />
-    </div>
-  );
-}
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating
-      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-      defaultRating={3}
-    />
-    <Test />
+    <StarRating messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]} />
   </React.StrictMode>
 );
