6964f8bad1eb3335967ff81dade0ffa62e33b682
CharlesMariga <charlesmariga37@gmail.com>
2023-12-11 10:10:07 +0300

    refs to select DOM elements

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,25 @@
+import { useEffect, useRef } from "react";
+
 export default function Search({ query, setQuery }) {
+  const inputEl = useRef(null);
+
+  useEffect(() => {
+    inputEl.current.focus();
+
+    function callBack(e) {
+      if (document.activeElement === inputEl.current) return;
+      if (e.code === "Enter") {
+        inputEl.current.focus();
+        setQuery("");
+      }
+    }
+    document.addEventListener("keypress", callBack);
+    return () => document.removeEventListener("keypress", callBack);
+  }, [setQuery]);
+
   return (
     <input
+      ref={inputEl}
       className="search"
       type="text"
       placeholder="Search movies..."
